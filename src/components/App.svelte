2e3e47767cf74d20916dbfc2d7f96d04574be84f
Antoine <antoine.carre@imt-atlantique.net>
2024-02-29 13:00:48 -0800

    save(not working)

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,8 +1,17 @@
 <script>
   import Scroller from "@sveltejs/svelte-scroller";
+  import BarGraph1 from '../components/BarGraph1.svelte';
+  //import BarGraph2 from '../components/BarGraph2.svelte';
+  import {onMount} from 'svelte';
   
   let count, index, offset, progress;
   let width, height;
+  let data1 = [];
+onMount(async () => {
+  const res = fetch('data_cleaned.csv'); 
+  const csv = res.text();
+  data1 = d3.csvParse(csv, d3.autoType)});
+
 </script>
 <main>
 <Scroller
@@ -17,20 +26,10 @@
 <div 
   class="background" slot="background" 
   bind:clientWidth={width} bind:clientHeight={height}>
-  <Map bind:geoJsonToFit {index} />
-  <Graph {index} {width} {height} {projection} />
-  <div class="progress-bars">
-    <p>current section: <strong>{index + 1}/{count}</strong></p>
-    <progress value={count ? (index + 1) / count : 0} />
-    <p>offset in current section</p>
-    <progress value={offset || 0} />
-    <p>total progress</p>
-    <progress value={progress || 0} />
-    </div>
 </div>
 <div class="foreground" slot="foreground">
   <section>This is the first section.</section>
-  <section>This is the second section.</section>
+  <section>This is the second section.<BarGraph1/></section>
   <section>This is the third section.</section>
   <section>This is the fourth section.</section>
 </div>
@@ -42,30 +41,25 @@
     width: 100%;
     height: 100vh;
     position: relative;
+    background-color: rgba(0, 0, 0, 0.2); /* 20% opaque */
     outline: green solid 3px;
   }
 
   .foreground {
-    width: 50%;
+    width: 70%;
     margin: 0 auto;
     height: auto;
     position: relative;
     outline: red solid 3px;
   }
 
-  .progress-bars {
-    position: absolute;
-    background: rgba(170, 51, 120, 0.2) /*  40% opaque */;
-    visibility: visible;
-  }
-
   section {
     height: 80vh;
-    background-color: rgba(0, 0, 0, 0.2); /* 20% opaque */
+    background-color: white; /* 20% opaque */
     /* color: white; */
     outline: magenta solid 3px;
     text-align: center;
-    max-width: 750px; /* adjust at will */
+    max-width: 2000px; /* adjust at will */
     color: black;
     padding: 1em;
     margin: 0 0 2em 0;
---
+++ src/components/BarGraph2.svelte
@@ -0,0 +1,127 @@
+<script>
+    import { Chart, Card, Dropdown, DropdownItem } from 'flowbite-svelte';
+  
+    const options = {
+      series: [
+        {
+          name: 'Income',
+          color: '#31C48D',
+          data: ['1420', '1620', '1820', '1420', '1650', '2120']
+        },
+        {
+          name: 'Expense',
+          data: ['788', '810', '866', '788', '1100', '1200'],
+          color: '#F05252'
+        }
+      ],
+      chart: {
+        sparkline: {
+          enabled: false
+        },
+        type: 'bar',
+        width: '100%',
+        height: 400,
+        toolbar: {
+          show: false
+        }
+      },
+      fill: {
+        opacity: 1
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+          columnWidth: '100%',
+          borderRadiusApplication: 'end',
+          borderRadius: 6,
+          dataLabels: {
+            position: 'top'
+          }
+        }
+      },
+      legend: {
+        show: true,
+        position: 'bottom'
+      },
+      dataLabels: {
+        enabled: false
+      },
+      tooltip: {
+        shared: true,
+        intersect: false,
+        formatter: function (value) {
+          return '$' + value;
+        }
+      },
+      xaxis: {
+        labels: {
+          show: true,
+          style: {
+            fontFamily: 'Inter, sans-serif',
+            cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+          },
+          formatter: function (value) {
+            return '$' + value;
+          }
+        },
+        categories: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        axisTicks: {
+          show: false
+        },
+        axisBorder: {
+          show: false
+        }
+      },
+      yaxis: {
+        labels: {
+          show: true,
+          style: {
+            fontFamily: 'Inter, sans-serif',
+            cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+          }
+        }
+      },
+      grid: {
+        show: true,
+        strokeDashArray: 4,
+        padding: {
+          left: 2,
+          right: 2,
+          top: -20
+        }
+      }
+    };
+  </script>
+  
+  <Card>
+    <div class="flex justify-between border-gray-200 border-b dark:border-gray-700 pb-3">
+      <dl>
+        <dt class="text-base font-normal text-gray-500 dark:text-gray-400 pb-1">Profit</dt>
+        <dd class="leading-none text-3xl font-bold text-gray-900 dark:text-white">$5,405</dd>
+      </dl>
+    </div>
+  
+    <div class="grid grid-cols-2 py-3">
+      <dl>
+        <dt class="text-base font-normal text-gray-500 dark:text-gray-400 pb-1">Income</dt>
+        <dd class="leading-none text-xl font-bold text-green-500 dark:text-green-400">$23,635</dd>
+      </dl>
+      <dl>
+        <dt class="text-base font-normal text-gray-500 dark:text-gray-400 pb-1">Expense</dt>
+        <dd class="leading-none text-xl font-bold text-red-600 dark:text-red-500">-$18,230</dd>
+      </dl>
+    </div>
+  
+    <Chart {options} />
+    <div class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
+      <div class="flex justify-between items-center pt-5">
+        <Dropdown class="w-40" offset="-6">
+          <DropdownItem>Yesterday</DropdownItem>
+          <DropdownItem>Today</DropdownItem>
+          <DropdownItem>Last 7 days</DropdownItem>
+          <DropdownItem>Last 30 days</DropdownItem>
+          <DropdownItem>Last 90 days</DropdownItem>
+        </Dropdown>
+      </div>
+    </div>
+  </Card>
