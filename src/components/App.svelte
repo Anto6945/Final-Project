1cb31bb33c896ffe223daf65f55e7809fdef3920
Antoine <antoine.carre@imt-atlantique.net>
2024-02-28 19:33:15 -0800

    test1

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,73 @@
 <script>
   // Write your JS here, or import other files
+  import Scroller from "@sveltejs/svelte-scroller";
+  
 </script>
-
 <main>
-  <h1>Svelte template</h1>
-
-  <p>Write your HTML here</p>
+<Scroller
+  top={0.0}
+  bottom={1}
+  threshold={0.5}
+  bind:count
+  bind:index
+  bind:offset
+  bind:progress
+>
+<div 
+  class="background" slot="background" 
+  bind:clientWidth={width} bind:clientHeight={height}>
+  <Map bind:geoJsonToFit {index} />
+  <Graph {index} {width} {height} {projection} />
+  <div class="progress-bars">
+    <p>current section: <strong>{index + 1}/{count}</strong></p>
+    <progress value={count ? (index + 1) / count : 0} />
+    <p>offset in current section</p>
+    <progress value={offset || 0} />
+    <p>total progress</p>
+    <progress value={progress || 0} />
+    </div>
+</div>
+<div class="foreground" slot="foreground">
+  <section>This is the first section.</section>
+  <section>This is the second section.</section>
+  <section>This is the third section.</section>
+  <section>This is the fourth section.</section>
+</div>
+</Scroller>
 </main>
 
 <style>
-  /* Write your CSS here */
+  .background {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    outline: green solid 3px;
+  }
+
+  .foreground {
+    width: 50%;
+    margin: 0 auto;
+    height: auto;
+    position: relative;
+    outline: red solid 3px;
+  }
+
+  .progress-bars {
+    position: absolute;
+    background: rgba(170, 51, 120, 0.2) /*  40% opaque */;
+    visibility: visible;
+  }
+
+  section {
+    height: 80vh;
+    background-color: rgba(0, 0, 0, 0.2); /* 20% opaque */
+    /* color: white; */
+    outline: magenta solid 3px;
+    text-align: center;
+    max-width: 750px; /* adjust at will */
+    color: black;
+    padding: 1em;
+    margin: 0 0 2em 0;
+  }
+
 </style>
