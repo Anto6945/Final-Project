1bbd655cfc18a70aea9861b45e73e00135784ea1
aadhyanav <126607197+aadhyanav@users.noreply.github.com>
2024-03-01 13:35:05 -0800

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,9 @@
+<!-- Your main Svelte file -->
 <script>
   import Scroller from "@sveltejs/svelte-scroller";
   import BarGraph1 from './BarGraph1.svelte';
   import BarGraph2 from './BarGraph2.svelte';
+  import Writeup from './Writeup.svelte';
   import {onMount} from 'svelte';
   import Map from './Map.svelte';
   import * as d3 from 'd3';
@@ -9,46 +11,48 @@
   let count, index, offset, progress;
   let width, height;
   let data1 = [];
-onMount(async () => {
-  function getTop10Objects(arr, property) {
-    // Sort the array of objects by the specified property in descending order
-    const sortedArr = arr.slice().sort((a, b) => b[property] - a[property]);
+  
+  onMount(async () => {
+    function getTop10Objects(arr, property) {
+      // Sort the array of objects by the specified property in descending order
+      const sortedArr = arr.slice().sort((a, b) => b[property] - a[property]);
 
-    // Return the top 10 objects
-    return sortedArr.slice(0, 10);
-  }
-  const res = await fetch('data_cleaned.csv'); 
-  const csv = await res.text();
-  data1 = d3.csvParse(csv, d3.autoType)
-  console.log(data1)
-  const top10Objects = getTop10Objects(data1, '1995');
-  console.log(top10Objects);
-});
-
+      // Return the top 10 objects
+      return sortedArr.slice(0, 10);
+    }
+    const res = await fetch('data_cleaned.csv'); 
+    const csv = await res.text();
+    data1 = d3.csvParse(csv, d3.autoType);
+    console.log(data1);
+    const top10Objects = getTop10Objects(data1, '1995');
+    console.log(top10Objects);
+  });
 </script>
-<main>
-<Scroller
-  top={0.0}
-  bottom={1}
-  threshold={0.5}
-  bind:count
-  bind:index
-  bind:offset
-  bind:progress
->
 
-<div 
-  class="background" slot="background" 
-  bind:clientWidth={width} bind:clientHeight={height}>
-</div>
-<div class="foreground" slot="foreground">
-  <section>Map Interactive.<Map/></section>
-  <section>Bar Chart showing the top 10 most traveled countries!<BarGraph1/></section>
-  <section>Bar Chart showing the top 10 least traveled countries!<BarGraph2/></section>
-  <section>This is the fourth section.</section>
-</div>
-</Scroller>
+<main>
+  <Scroller
+    top={0.0}
+    bottom={1}
+    threshold={0.5}
+    bind:count
+    bind:index
+    bind:offset
+    bind:progress
+  >
+
+    <div 
+      class="background" slot="background" 
+      bind:clientWidth={width} bind:clientHeight={height}
+    ></div>
+    <div class="foreground" slot="foreground">
+      <section>Map Interactive.<Map/></section>
+      <section>Bar Chart showing the top 10 most traveled countries!<BarGraph1/></section>
+      <section>Bar Chart showing the top 10 least traveled countries!<BarGraph2/></section>
+      <Writeup/>
+    </div>
+  </Scroller>
 </main>
+
 
 <style>
   .background {
