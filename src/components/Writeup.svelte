bb0f580aa3810a2a6c461980633cf3ead2dc45ad
aadhyanav <126607197+aadhyanav@users.noreply.github.com>
2024-03-01 13:42:00 -0800

    Update Writeup.svelte

--- src/components/Writeup.svelte
+++ src/components/Writeup.svelte
@@ -1,3 +1,8 @@
 <main>
-    <p> We have created a scroller that includes 3 of our planned visualizations. </p>
+    <p> We have created a scroller that includes 3 of our planned visualizations. 
+        We have created the seperate files in Svelte and linked them all to the main App.Svelte file.
+        So far, we have cleaned our data by changing the number to millions and our bar graph has a slider
+        that enables a user to pick the year. The most challenging part of the project is most probably going 
+        to be the visualization. 
+    </p>
 </main>
