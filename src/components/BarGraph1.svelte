bf24dc0aaa8fae7c33b196a66fce1a19747fdc98
Antoine <antoine.carre@imt-atlantique.net>
2024-03-01 12:02:26 -0800

    Revert "interactive"
    This reverts commit ad948a44eca6bbd98aa2b7a182939cb1995ba538.

--- src/components/BarGraph1.svelte
+++ src/components/BarGraph1.svelte
@@ -1,26 +1,30 @@
 <script>
+	
   import { datatop } from '../lib/data';
   import BarChart from './BarChart.svelte';
+  
 </script>
 
 <div class="app">
   <BarChart 
-    datatop={datatop.sort((a, b) => a['2020'] - b['2020'])} 
-  />
+        datatop={datatop.sort(
+      (a, b) => a['2020'] - b['2020'],
+    )} 
+    />
 </div>
 
 <style>
-  @import url("https://fonts.googleapis.com/css?family=Merriweather:700&display=swap");
-  @import url("https://fonts.googleapis.com/css?family=Lato:400&display=swap");
-  
-  :global(body) {
-    color: #282828;
-    background: #ffffff;
-    font-family: "Lato", sans-serif;
-    font-size: 14px;
+    @import url("https://fonts.googleapis.com/css?family=Merriweather:700&display=swap");
+    @import url("https://fonts.googleapis.com/css?family=Lato:400&display=swap");
+    
+    :global(body) {
+      color: #282828;
+        background: #ffffff;
+        font-family: "Lato", sans-serif;
+        font-size: 14px;
     font-weight: 400;
     line-height: 24px;
-  }
+    }
 
   .app {
     display: flex;
@@ -28,6 +32,6 @@
     align-items: center;
     height: 100%;
     width: 100%;
-    margin: 0 15px;
+        margin: 0 15px;
   }
 </style>
