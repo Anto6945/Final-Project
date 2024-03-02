f8a822737c12c80fb8babf047947345d6f594cfa
aadhyanav <126607197+aadhyanav@users.noreply.github.com>
2024-03-01 16:06:34 -0800

    Update Writeup.svelte

--- src/components/Writeup.svelte
+++ src/components/Writeup.svelte
@@ -1,13 +1,13 @@
 <main>
     <p> We have created a scroller that includes 3 of our planned visualizations. 
-        We have created the separate files in Svelte and linked them all to the main App.Svelte file.
-        So far, we have cleaned our data by changing the number of arrivals to millions and our bar graph has a slider
-        that enables a user to pick the year. We've done the basic structure of our project with very simple 
-        text and graphs. </p>
+        Using the template, we created seperate svelte files for each of our visualizations and linked it to our main App.Svelte file. 
+        So far, we have cleaned our data by changing the number of arrivals to millions and our two bar graphs has a slider
+        that enables a user to pick the year to see the most and least popular countries for international travel. We've finished the basic structure of our project with very simple 
+        text and graphs, which we anticipate will have more interactions and details for the final protoype. </p>
     <p> The most challenging part of the project is most probably going to be the depicting the storytelling
-        part of the project well. Adding the right animations and information to make our project engaging
-        and informative, might take us time to perfect. I think the animations might be a little difficult 
+        aspect of the project. We think that integrating the appropriate animations and information to make our 
+        project engaging and informative might require lots of time and effort. The animations might be a little difficult 
         because both of us are not too sure how to do them in javascript or svelte. We will have to do additional
         research about international tourism to add annotations about events like COVID-19 or conflicts that have
-        impacted travel. </p> 
+        impacted travel. Also adding features that are make our projecy more unique and creative might be challening for us. </p> 
 </main>
