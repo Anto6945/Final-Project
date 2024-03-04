3534ed069ba4fbfce9f3230531c5f8df5448f7f4
aadhyanav <126607197+aadhyanav@users.noreply.github.com>
2024-03-04 08:08:16 -0800

    Update Writeup.svelte

--- src/components/Writeup.svelte
+++ src/components/Writeup.svelte
@@ -4,10 +4,13 @@
         So far, we have cleaned our data by changing the number of arrivals to millions and our two bar graphs has a slider
         that enables a user to pick the year to see the most and least popular countries for international travel. We've finished the basic structure of our project with very simple 
         text and graphs, which we anticipate will have more interactions and details for the final protoype. </p>
-    <p> The most challenging part of the project is most probably going to be the depicting the storytelling
-        aspect of the project. We think that integrating the appropriate animations and information to make our 
-        project engaging and informative might require lots of time and effort. The animations might be a little difficult 
-        because both of us are not too sure how to do them in javascript or svelte. We will have to do additional
-        research about international tourism to add annotations about events like COVID-19 or conflicts that have
-        impacted travel. Also adding features that are make our projecy more unique and creative might be challening for us. </p> 
+    <p> The most challenging part of the project is most probably going to be the 
+        depicting the storytelling aspect of the project. We think that integrating 
+        the appropriate animations and information to make our project engaging and 
+        informative might require lots of time and effort. The animations might be a 
+        little difficult because both of us are not too sure how to do them in 
+        javascript or svelte. We will have to do additional research about international 
+        tourism to add annotations about events like COVID-19 or conflicts that have 
+        impacted travel. Also adding features that are make our project more unique and 
+        creative might be challenging for us. </p> 
 </main>
