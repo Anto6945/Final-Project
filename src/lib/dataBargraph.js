3c6f42d8ec0122b979a01559242562a011ffe456
Antoine <antoine.carre@imt-atlantique.net>
2024-03-05 15:52:47 -0800

    test

---
+++ src/lib/dataBargraph.js
@@ -0,0 +1,65 @@
+ dataBargraph = [
+  {
+    Rank: "1",
+    City: "London–Heathrow, United Kingdom",
+    Passengers: "182,222",
+    Carriers: "British Airways"
+  },
+  {
+    Rank: "2",
+    City: "San José del Cabo, Mexico",
+    Passengers: "131,967",
+    Carriers: "Alaska, Southwest"
+  },
+  {
+    Rank: "3",
+    City: "Vancouver, Canada",
+    Passengers: "119,760",
+    Carriers: "Air Canada, WestJet"
+  },
+  {
+    Rank: "4",
+    City: "Toronto–Pearson, Canada",
+    Passengers: "109,033",
+    Carriers: "Air Canada"
+  },
+  {
+    Rank: "5",
+    City: "Munich, Germany",
+    Passengers: "96,757",
+    Carriers: "Lufthansa"
+  },
+  {
+    Rank: "6",
+    City: "Tokyo–Narita, Japan",
+    Passengers: "63,844",
+    Carriers: "Japan Airlines"
+  },
+  {
+    Rank: "7",
+    City: "Calgary, Canada",
+    Passengers: "54,589",
+    Carriers: "WestJet"
+  },
+  {
+    Rank: "6",
+    City: "Puerto Vallarta, Mexico",
+    Passengers: "44,847",
+    Carriers: "Alaska"
+  },
+  {
+    Rank: "9",
+    City: "Montréal–Trudeau, Canada",
+    Passengers: "44,287",
+    Carriers: "Air Canada"
+  },
+  {
+    Rank: "10",
+    City: "Cancún, Mexico",
+    Passengers: "5,849",
+    Carriers: "Alaska"
+  },
+  {
+    Rank: ""
+  }
+];
