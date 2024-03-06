3740f463490f3f53772c3c013140620d1514821b
aadhyanav <126607197+aadhyanav@users.noreply.github.com>
2024-03-05 17:08:44 -0800

    Update Linegraph.svelte

--- src/components/Linegraph.svelte
+++ src/components/Linegraph.svelte
@@ -1,12 +1,13 @@
 <script>
+    import { afterUpdate } from 'svelte';
     import * as d3 from 'd3';
-    import { data_lineGraph } from '../lib/data_lineGraph';
+    import { data_lineGraph } from '../lib/data_lineGraph'; // Importing the data from the file
     
     let margin = { top: 20, right: 20, bottom: 30, left: 50 };
     let width = 600 - margin.left - margin.right;
     let height = 400 - margin.top - margin.bottom;
     
-    onMount(() => {
+    function drawGraph() {
       const svg = d3.select('svg');
       
       let x = d3.scaleLinear().range([0, width]);
@@ -28,14 +29,14 @@
         .call(d3.axisBottom(x));
     
       svg.append('path')
-        .datum(data)
+        .datum(data_lineGraph)
         .attr('fill', 'none')
         .attr('stroke', 'steelblue')
         .attr('stroke-width', 2)
         .attr('d', line);
     
       svg.selectAll('circle')
-        .data(data)
+        .data(data_lineGraph)
         .enter()
         .append('circle')
         .attr('cx', d => x(d.Year))
@@ -43,14 +44,18 @@
         .attr('r', 5)
         .attr('fill', 'blue')
         .on('click', d => alert(`Number: ${d.Total}`));
-    });
-</script>
-
-<style>
-  svg {
-    width: 600px;
-    height: 400px;
-  }
-</style>
-
-<svg></svg>
+    }
+  
+    // Call the drawGraph function after the component is updated
+    afterUpdate(drawGraph);
+  </script>
+  
+  <style>
+    svg {
+      width: 600px;
+      height: 400px;
+    }
+  </style>
+  
+  <svg></svg>
+  
