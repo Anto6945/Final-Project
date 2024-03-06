390f8165ca21d8ab311d75ce35ceb431c6bd4cca
Antoine <antoine.carre@imt-atlantique.net>
2024-03-06 12:31:50 -0800

    pieChart

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,89 +1,89 @@
-<!-- Your main Svelte file -->
 <script>
-  import Scroller from "@sveltejs/svelte-scroller";
-  import BarGraph1 from './BarGraph1.svelte';
-  import BarGraph2temporaire from "./BarGraph2temporaire.svelte";
-  import Map from './Map.svelte';
-  import {onMount} from 'svelte';
   import * as d3 from 'd3';
-  
-  let count, index, offset, progress;
-  let width, height;
-  let data1 = [];
-  
-  onMount(async () => {
-    function getTop10Objects(arr, property) {
-      // Sort the array of objects by the specified property in descending order
-      const sortedArr = arr.slice().sort((a, b) => b[property] - a[property]);
+  import {data_pie_chart} from '../lib/data_pie_chart';
 
-      // Return the top 10 objects
-      return sortedArr.slice(0, 10);
-    }
-    const res = await fetch('data_cleaned.csv'); 
-    const csv = await res.text();
-    data1 = d3.csvParse(csv, d3.autoType);
-    console.log(data1);
-    const top10Objects = getTop10Objects(data1, '1995');
-    console.log(top10Objects);
-  });
+  let arcGenerator = d3.arc()
+    .innerRadius(10)
+    .outerRadius(100)
+    .padAngle(.02)
+    .cornerRadius(4);
+  let recorded_mouse_position = {x: 0, y: 0};
+
+  let pieAngleGenerator = d3.pie().value( d => d[1] );
+let arc_data = []
+const arc_color = d3.scaleLinear()
+  .range(["#faffd1", "#db921d", "#b86a04", "#a65d29" ,"#6e3003"]);
+let hovered = -1;
+  $:{
+      arc_data = pieAngleGenerator(data_pie_chart);
+      console.log(arc_data);
+  }
 </script>
-
-<main>
-  <h1>Visualizing San Diego (SAN) Air Traffic </h1>
-  <p>  We aim to show the most common routes/destinations (domestically and internationally) and airlines that go through the San Diego airport. </p>
-  <Scroller
-    top={0.0}
-    bottom={1}
-    threshold={0.5}
-    bind:count
-    bind:index
-    bind:offset
-    bind:progress
-  >
-
-    <div 
-      class="background" slot="background" 
-      bind:clientWidth={width} bind:clientHeight={height}
-    ></div>
-    <div class="foreground" slot="foreground">
-      <section>Map Interactive.<Map/></section>
-      <section> United States Domestic Routes</section>
-      <p> The bar chart below shows the most popular countries for travel. As we can see from 2004 onwards, France tends to be the most popular country with the most number of arrivals!</p>
-      <section>Bar Chart showing the top 10 most traveled countries!<BarGraph1/></section>
-      <p> This visualization shows the countries that are the least popular among people to visit. Many of the countries are islands with small population and different countries in the African continent.</p>
-      <section>Bar Chart showing the top 10 least traveled countries!<BarGraph2temporaire/></section>
-    </div>
-  </Scroller>
-</main>
-
+<div class="visualization">
+<svg width="500" height="500">
+  <g transform="translate(250, 120)">
+    {#each arc_data as data, index}
+      <path
+        d={arcGenerator({
+          startAngle: data.startAngle,
+          endAngle: data.endAngle
+        })}
+        fill={index === hovered ? "brown": arc_color(index)}
+        on:mouseover={(event) => { hovered = index; 
+        recorded_mouse_position = {
+          x: event.pageX,
+          y: event.pageY
+          }}}
+        on:mouseout={(event) => { hovered = -1; }}
+      />
+      <text
+        x={arcGenerator.centroid(data)[0]}   
+        y={arcGenerator.centroid(data)[1]}                      
+        text-anchor="middle"                
+        alignment-baseline="middle"         
+      >
+        {data.data[0]}        
+      </text>
+    {/each}		
+  </g>
+</svg>
+<div class={hovered === -1 ? "tooltip-hidden": "tooltip-visible"}
+  style="left: {recorded_mouse_position.x + 40}px; top:
+  {recorded_mouse_position.y + 40}px">
+  {#if hovered !== -1}
+    There {arc_data[hovered].data[1] === 1 ? "is" : "are"}
+    {arc_data[hovered].data[1]}
+    record{arc_data[hovered].data[1] === 1 ? "" : "s"}
+    where you have "{arc_data[hovered].data[0]}" todo items.
+  {/if}
+</div>
+</div>
 
 <style>
-  .background {
-    width: 100%;
-    height: 100vh;
-    position: relative;
-    background-color: rgba(0, 0, 0, 0); /* 20% opaque */
-    outline: white 3px;
-  }
+.visualization {
+  font: 25px sans-serif;
+  margin: auto;
+  margin-top: 1px;
+  text-align: middle;
+}
 
-  .foreground {
-    width: 100%;
-    margin: 0 auto;
-    height: auto;
-    position: relative;
-    outline: white 3px;
-  }
+/* dynamic classes for the tooltip */
+.tooltip-hidden {
+  visibility: hidden;
+  font-family: "Nunito", sans-serif;
+  width: 200px;
+  position: absolute;
+}
 
-  section {
-    height: 80vh;
-    background-color: white; /* 20% opaque */
-    /* color: white; */
-    outline: white 3px;
-    text-align: center;
-    max-width: 2000px; /* adjust at will */
-    color: black;
-    padding: 1em;
-    margin: 0 0 2em 0;
-  }
-
+.tooltip-visible {
+  font: 25px sans-serif;
+  font-family: "Nunito", sans-serif;
+  visibility: visible;
+  background-color: #f0dba8;
+  border-radius: 10px;
+  width: 200px;
+  color: black;
+  position: absolute;
+  padding: 10px;
+}
 </style>
