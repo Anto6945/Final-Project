270784786a3e21d1b4b40ffb3084dd4cb5d968bc
Antoine <antoine.carre@imt-atlantique.net>
2024-03-06 18:39:41 -0800

    data changes

--- src/lib/data_BarGraphUS.js
+++ src/lib/data_BarGraphUS.js
@@ -3,60 +3,80 @@
     Rank: "1",
     City: "Las Vegas, Nevada",
     Passengers: "794000",
-    Carriers: "Delta, Frontier, Southwest, Spirit"
+    Carriers: "Delta, Frontier, Southwest, Spirit",
+    CoordinatesN: "36.0851",
+    CoordinatesW: "115.1514"
   },
   {
     Rank: "2",
     City: "Denver, Colorado",
     Passengers: "730000",
-    Carriers: "Frontier, Southwest, United"
+    Carriers: "Frontier, Southwest, United",
+    CoordinatesN: "39.8561",
+    CoordinatesW: "104.6737"
   },
   {
     Rank: "3",
     City: "Phoenix, Arizona",
     Passengers: "632000",
-    Carriers: "American, Frontier, Southwest"
+    Carriers: "American, Frontier, Southwest",
+    CoordinatesN: "33.4342",
+    CoordinatesW: "112.0083"
   },
   {
     Rank: "4",
     City: "San Francisco, California",
     Passengers: "625000",
-    Carriers: "Alaska, Southwest, United"
+    Carriers: "Alaska, Southwest, United",
+    CoordinatesN: "37.6215",
+    CoordinatesW: "122.3790"
   },
   {
     Rank: "5",
     City: "Seattle, Washington",
     Passengers: "624000",
-    Carriers: "Alaska, Delta"
+    Carriers: "Alaska, Delta",
+    CoordinatesN: "47.4502",
+    CoordinatesW: "122.3088"
   },
   {
     Rank: "6",
     City: "San Jose, California",
     Passengers: "602000",
-    Carriers: "Alaska, Southwest, Spirit"
+    Carriers: "Alaska, Southwest, Spirit",
+    CoordinatesN: "37.3639",
+    CoordinatesW: "121.9289"
   },
   {
     Rank: "7",
     City: "Sacramento, California",
     Passengers: "594000",
-    Carriers: "Alaska, Southwest, Spirit"
+    Carriers: "Alaska, Southwest, Spirit",
+    CoordinatesN: "38.6957",
+    CoordinatesW: "121.5908"
   },
   {
     Rank: "8",
     City: "Dallas/Fort Worth, Texas",
     Passengers: "575000",
-    Carriers: "American, Frontier"
+    Carriers: "American, Frontier",
+    CoordinatesN: "32.8998",
+    CoordinatesW: "97.0403"
   },
   {
     Rank: "9",
     City: "Oakland, California",
     Passengers: "415000",
-    Carriers: "Southwest, Spirit"
+    Carriers: "Southwest, Spirit",
+    CoordinatesN: "37.7214",
+    CoordinatesW: "122.2208"
   },
   {
     Rank: "10",
-    City: "Chicago–O'Hare, Illinois",
+    City: "Chicago, Illinois",
     Passengers: "398000",
-    Carriers: "American, United"
+    Carriers: "American, United",
+    CoordinatesN: "41.9742",
+    CoordinatesW: "87.9073"
   },
 ];
