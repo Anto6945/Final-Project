46ba560641bcaba6e92e1d098182a2b5f08b82a0
aadhyanav <126607197+aadhyanav@users.noreply.github.com>
2024-03-07 11:36:41 -0800

    intro

---
+++ src/components/Introduction.svelte
@@ -0,0 +1,27 @@
+<div class="header-text">
+    <p>Ever wonder what the San Diego International Airport (SAN) Air Traffic is Like?</p>
+ </div>
+ 
+ 
+ <script>
+    // JavaScript code can go here if needed
+ </script>
+ 
+ 
+ <style>
+    .header-text {
+        text-align: center;
+        font-size: 22px;
+        font-weight: bold;
+        margin: 20px auto; /* Center the text horizontally */
+        border: 2px solid gold; /* Add a border around the text */
+        padding: 10px; /* Add padding to create space between text and border */
+        font-family: Arial, sans-serif; /* Change font to Arial */
+        display: inline-block; /* Make the element behave like an inline block */
+        max-width: 80%; /* Set a maximum width for the container */
+    }
+ </style>
+ 
+ 
+ 
+ 
