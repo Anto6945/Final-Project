702f2fed7553f106c7c43a01782f8aade0b1486c
aadhyanav <126607197+aadhyanav@users.noreply.github.com>
2024-03-07 11:35:21 -0800

    change

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -4,32 +4,33 @@
   import BarGraph1 from './BarGraph1.svelte';
   import BarGraphUS from './BarGraphUS.svelte';
   import Linegraph from "./Linegraph.svelte";
-  import Pie from "./Pie.svelte";
+  import PieChart from "./PieChart.svelte";
+  import Introduction from "./Introduction.svelte";
   import {onMount} from 'svelte';
   import * as d3 from 'd3';
-  
-  let count, index, offset, progress;
+   let count, index, offset, progress;
   let width, height;
   let data1 = [];
-  
-  onMount(async () => {
+   onMount(async () => {
     function getTop10Objects(arr, property) {
       // Sort the array of objects by the specified property in descending order
       const sortedArr = arr.slice().sort((a, b) => b[property] - a[property]);
-
+ 
+ 
       // Return the top 10 objects
       return sortedArr.slice(0, 10);
     }
-    const res = await fetch('data_cleaned.csv'); 
+    const res = await fetch('data_cleaned.csv');
     const csv = await res.text();
     data1 = d3.csvParse(csv, d3.autoType);
     console.log(data1);
     const top10Objects = getTop10Objects(data1, '1995');
     console.log(top10Objects);
   });
-</script>
-
-<main>
+ </script>
+ 
+ 
+ <main>
   <h1 class="custom-heading">Visualizing San Diego Airport (SAN) Air Traffic</h1>
   <Scroller
     top={0.0}
@@ -40,23 +41,27 @@
     bind:offset
     bind:progress
   >
-
-    <div 
-      class="background" slot="background" 
+ 
+ 
+    <div
+      class="background" slot="background"
       bind:clientWidth={width} bind:clientHeight={height}
     ></div>
     <div class="foreground" slot="foreground">
+      <section><Introduction/></section>
       <section><Linegraph/></section>
-      <section><Pie/></section>
+      <section><PieChart/></section>
       <section><BarGraphUS/></section>
       <section><BarGraph1/></section>
-      
+     
     </div>
   </Scroller>
-</main>
-
-
-<style>
+ </main>
+ 
+ 
+ 
+ 
+ <style>
   .background {
     width: 100%;
     height: 100vh;
@@ -64,7 +69,8 @@
     background-color: rgba(0, 0, 0, 0); /* 20% opaque */
     outline: white 3px;
   }
-
+ 
+ 
   .foreground {
     width: 100%;
     margin: 0 auto;
@@ -72,7 +78,8 @@
     position: relative;
     outline: white 3px;
   }
-
+ 
+ 
   section {
     height: 80vh;
     background-color: white; /* 20% opaque */
@@ -88,7 +95,12 @@
     font-size: 34px; /* Adjust font size as needed */
     font-weight: bold; /* Optionally, adjust font weight */
     color: #333; /* Optionally, specify font color */
-    text-align: center; 
+    text-align: center;
   }
-
-</style>
+ 
+ 
+ </style>
+ 
+ 
+ 
+ 
--- src/components/pieChart.svelte
+++ src/components/pieChart.svelte
@@ -1,39 +1,42 @@
 <script>
     import * as d3 from 'd3';
     import { data_pie_chart } from '../lib/data_pie_chart';
-	let selectedYear = 2022;
+    let selectedYear = 2022;
     let arcGenerator = d3.arc()
         .innerRadius(10)
         .outerRadius(200)
         .padAngle(.02)
         .cornerRadius(4);
-
+ 
+ 
     let recorded_mouse_position = { x: 0, y: 0 };
     let hovered = -1;
     let arc_data = [];
-
+ 
+ 
     // Define the color scale with different colors for each section
     const arc_color = d3.scaleOrdinal()
         .domain(data_pie_chart[selectedYear].map(d => d.Airline))
         .range(d3.schemeCategory10); // You can use any color scheme you prefer
-
+ 
+ 
     $: {
         arc_data = d3.pie()
             .value(d => d.Share)
             (data_pie_chart[selectedYear]);
     }
-</script>
-<div class="overlay">
-	<label for="slider">Years {selectedYear}</label>
-	<input
-		id="slider"
-		type="range"
-		min="2018"
-		max="2022"
-		bind:value={selectedYear}
-	/>
-</div>
-<div class="visualization">
+ </script>
+ <div class="overlay">
+    <label for="slider">Years {selectedYear}</label>
+    <input
+        id="slider"
+        type="range"
+        min="2018"
+        max="2022"
+        bind:value={selectedYear}
+    />
+ </div>
+ <div class="visualization">
     <svg width="500" height="500">
         <!-- Title for the graph -->
         <text x="250" y="30" text-anchor="middle" font-size="20px" font-weight="bold">Airline distribution for San Diego Airport (2018-2022)</text>
@@ -54,24 +57,37 @@
                     text-anchor="middle"
                     alignment-baseline="middle"
                     font-size="13.5px"
-					style="font-family: Georgia;"
+                    style="font-family: Georgia;"
                 >
                     {data.data.Airline.replace(' Airlines', '')} <!-- Remove 'Airlines' from the text -->
                 </text>
             {/each}
         </g>
     </svg>
-
+ 
+ 
     <div class={hovered === -1 ? "tooltip-hidden" : "tooltip-visible"}
         style="left: {recorded_mouse_position.x + 40}px; top: {recorded_mouse_position.y + 40}px">
         {#if hovered !== -1}
             {arc_data[hovered].data.Share}% of the total flights are {arc_data[hovered].data.Airline == "Other Airlines" ? "other" : arc_data[hovered].data.Airline}.
         {/if}
     </div>
-</div>
-
-<style>
-	.overlay {
+    <div class="info-box">
+        <p> As we can see, Southwest has been dominating consistently in terms of being the most flown airline in the airspace.
+            Among all the years in the visualization, we see that it has the largest share among all airlines.
+        </p>
+        <p>We notice something interesting that <strong>majority of the flights passing through the San Diego airport are domestic
+            flights.</strong> Delta, United, Southwest, etc. that seem to be the leading flights are all U.S nationally owned airlines. Even if you
+            change the year, the "other" portion indicating various international airlines only seem to make up a really small
+            portion (less than 25 percent) of the pie chart. </p>
+        <p> What does this tell us about SAN airport? Many people use it for connecting flights and to travel domestically within the U.S.
+            While people a good amount of passengers do international travel, domestic travel seems to dominate by a large margin. </p>
+    </div>
+ </div>
+ 
+ 
+ <style>
+    .overlay {
         text-align: left;
     }
     .visualization {
@@ -80,7 +96,8 @@
         margin-top: 1px;
         text-align: left;
     }
-
+ 
+ 
     /* dynamic classes for the tooltip */
     .tooltip-hidden {
         visibility: hidden;
@@ -88,7 +105,8 @@
         width: 200px;
         position: absolute;
     }
-
+ 
+ 
     .tooltip-visible {
         font: 25px sans-serif;
         font-family: "Nunito", sans-serif;
@@ -100,4 +118,17 @@
         position: absolute;
         padding: 10px;
     }
-</style>
+    .info-box {
+        width: 450px;
+        padding: 20px;
+        background-color: #f9f1f9;
+        border: 2px solid #000000;
+        border-radius: 10px;
+        position: absolute;
+        top: 1530px; /* Adjust as needed */
+        right: 240px; /* Adjust as needed */
+        font-family: Arial, sans-serif; /* Change font to Arial */
+        font-size: 16px; /* Reduce font size */
+    }
+ </style>
+ 
