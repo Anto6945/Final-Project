6178ad431ea554dc419aa88b86cb4b298e40a8a2
Antoine <antoine.carre@imt-atlantique.net>
2024-03-06 18:46:35 -0800

    data changes

--- src/lib/dataBargraph.js
+++ src/lib/dataBargraph.js
@@ -3,60 +3,80 @@
     Rank: "1",
     City: "London–Heathrow, United Kingdom",
     Passengers: "182222",
-    Carriers: "British Airways"
+    Carriers: "British Airways",
+    CoordinatesN: "51.4700",
+     CoordinatesW: "0.4543"
   },
   {
     Rank: "2",
     City: "San José del Cabo, Mexico",
     Passengers: "131967",
-    Carriers: "Alaska, Southwest"
+    Carriers: "Alaska, Southwest",
+    CoordinatesN: "23.1625",
+     CoordinatesW: "109.7217"
   },
   {
     Rank: "3",
     City: "Vancouver, Canada",
     Passengers: "119760",
-    Carriers: "Air Canada, WestJet"
+    Carriers: "Air Canada, WestJet",
+    CoordinatesN: "49.2827",
+     CoordinatesW: "123.1207"
   },
   {
     Rank: "4",
     City: "Toronto–Pearson, Canada",
     Passengers: "109033",
-    Carriers: "Air Canada"
+    Carriers: "Air Canada",
+    CoordinatesN: "43.6777",
+     CoordinatesW: "79.6248"
   },
   {
     Rank: "5",
     City: "Munich, Germany",
     Passengers: "96757",
-    Carriers: "Lufthansa"
+    Carriers: "Lufthansa",
+    CoordinatesN: "48.3537",
+     CoordinatesW: "-11.7750"
   },
   {
     Rank: "6",
     City: "Tokyo–Narita, Japan",
     Passengers: "63844",
-    Carriers: "Japan Airlines"
+    Carriers: "Japan Airlines",
+    CoordinatesN: "35.7647",
+     CoordinatesW: "-140.3863"
   },
   {
     Rank: "7",
     City: "Calgary, Canada",
     Passengers: "54589",
-    Carriers: "WestJet"
+    Carriers: "WestJet",
+    CoordinatesN: "51.1314",
+     CoordinatesW: "114.0103"
   },
   {
     Rank: "6",
     City: "Puerto Vallarta, Mexico",
     Passengers: "44847",
-    Carriers: "Alaska"
+    Carriers: "Alaska",
+    CoordinatesN: "20.6801",
+     CoordinatesW: "105.2544"
   },
   {
     Rank: "9",
     City: "Montréal–Trudeau, Canada",
     Passengers: "44287",
-    Carriers: "Air Canada"
+    Carriers: "Air Canada",
+    CoordinatesN: "45.4577",
+     CoordinatesW: "73.7497"
   },
   {
     Rank: "10",
     City: "Cancún, Mexico",
     Passengers: "5849",
-    Carriers: "Alaska"
-  }
+    Carriers: "Alaska",
+    CoordinatesN: "21.1619",
+     CoordinatesW: "86.8515"
+  },
 ];
