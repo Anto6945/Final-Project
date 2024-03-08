c3a71689a6f546ac3c896c4b2d2839b2ed38fa21
Antoine <antoine.carre@imt-atlantique.net>
2024-03-08 13:57:23 -0800

    buttons

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,4 @@
 <script>
-  //<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
   import Scroller from "@sveltejs/svelte-scroller";
   import BarGraph1 from './BarGraph1.svelte';
   import BarGraphUS from './BarGraphUS.svelte';
@@ -11,6 +10,12 @@
   
   let count, index, offset, progress;
   let width, height;
+  let airlines = false;
+  let ftimeAirline = false;
+  let domestic = false;
+  let inter = false;
+  let barmap = true;
+
  </script>
  
  
@@ -34,18 +39,34 @@
     <div class="foreground" slot="foreground">
       <section><Introduction/></section>
       <section><Linegraph/></section>
+      <button class = "btn bg custom-button btn-petit" on:click={() => {airlines = !airlines; ftimeAirline = true}}>Airlines</button>
+      {#if ftimeAirline}
+      {#if airlines}
       <section><PieChart/></section>
+      {/if}
+      <button class = "btn bg custom-button btn-petit" on:click={() => domestic = !domestic}>Domestic Flight</button>
+      {#if domestic}
+      <button class = "btn bg custom-button btn-petit" on:click={() => barmap = !barmap}>{barmap ? "Map":"Barchart"}</button>
+      {#if barmap}
       <section><BarGraphUS/></section>
-      <section><BarGraph1/></section>
+      {:else}
       <section><MapUS/></section>
-      <section></section>
-      <section></section>
+      {/if}
+      {/if}
+      <button class = "btn bg custom-button btn-petit" on:click={() => inter = !inter}>International Flight</button>
+      {#if inter}
+      <button class = "btn bg custom-button btn-petit" on:click={() => barmap = !barmap}>{barmap ? "Map":"Barchart"}</button>
+      {#if barmap}
+      <section><BarGraph1/></section>
+      {:else}
       <section></section>
       <section></section>
       <section></section>
       <section></section>
       <section><Map/></section>
-
+      {/if}
+      {/if}
+      {/if}
 
     </div>
   </Scroller>
@@ -62,7 +83,12 @@
     background-color: rgba(0, 0, 0, 0); /* 20% opaque */
     outline: white 3px;
   }
- 
+ .btn-petit{
+  font-size: 12px;
+  padding: 7px 15px;
+  border-radius: 5px;
+}
+.custom-button { background-color: rgb(202, 202, 202); }
  
   .foreground {
     width: 100%;
@@ -90,6 +116,7 @@
     color: #333; /* Optionally, specify font color */
     text-align: center;
   }
+  @import '../../bootstrap/css/bootstrap.css';
  
  
  </style>
