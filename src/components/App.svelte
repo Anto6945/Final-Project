c0fa3e32223fde5db283ace4b69c264f9d4ffa71
Antoine <antoine.carre@imt-atlantique.net>
2024-03-07 18:28:13 -0800

    almost

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,106 +1,319 @@
-<!-- Your main Svelte file -->
 <script>
-  import Scroller from "@sveltejs/svelte-scroller";
-  import BarGraph1 from './BarGraph1.svelte';
-  import BarGraphUS from './BarGraphUS.svelte';
-  import Linegraph from "./Linegraph.svelte";
-  import PieChart from "./PieChart.svelte";
-  import Introduction from "./Introduction.svelte";
-  import {onMount} from 'svelte';
+	import { draw } from 'svelte/transition';
   import * as d3 from 'd3';
-   let count, index, offset, progress;
-  let width, height;
-  let data1 = [];
-   onMount(async () => {
-    function getTop10Objects(arr, property) {
-      // Sort the array of objects by the specified property in descending order
-      const sortedArr = arr.slice().sort((a, b) => b[property] - a[property]);
- 
- 
-      // Return the top 10 objects
-      return sortedArr.slice(0, 10);
-    }
-    const res = await fetch('data_cleaned.csv');
-    const csv = await res.text();
-    data1 = d3.csvParse(csv, d3.autoType);
-    console.log(data1);
-    const top10Objects = getTop10Objects(data1, '1995');
-    console.log(top10Objects);
-  });
- </script>
- 
- 
- <main>
-  <h1 class="custom-heading">Visualizing San Diego Airport (SAN) Air Traffic</h1>
-  <Scroller
-    top={0.0}
-    bottom={1}
-    threshold={0.5}
-    bind:count
-    bind:index
-    bind:offset
-    bind:progress
-  >
- 
- 
-    <div
-      class="background" slot="background"
-      bind:clientWidth={width} bind:clientHeight={height}
-    ></div>
-    <div class="foreground" slot="foreground">
-      <section><Introduction/></section>
-      <section><Linegraph/></section>
-      <section><PieChart/></section>
-      <section><BarGraphUS/></section>
-      <section><BarGraph1/></section>
-     
-    </div>
-  </Scroller>
- </main>
- 
- 
- 
- 
- <style>
-  .background {
-    width: 100%;
-    height: 100vh;
-    position: relative;
-    background-color: rgba(0, 0, 0, 0); /* 20% opaque */
-    outline: white 3px;
+  const data_lineGraph = [
+  {
+    Year: "1949",
+    Total: "139327"
+  },
+  {
+    Year: "1950",
+    Total: "193373"
+  },
+  {
+    Year: "1951",
+    Total: "321189"
+  },
+  {
+    Year: "1952",
+    Total: "390427"
+  },
+  {
+    Year: "1953",
+    Total: "402674"
+  },
+  {
+    Year: "1954",
+    Total: "426600"
+  },
+  {
+    Year: "1955",
+    Total: "496641"
+  },
+  {
+    Year: "1956",
+    Total: "582120"
+  },
+  {
+    Year: "1957",
+    Total: "682609"
+  },
+  {
+    Year: "1958",
+    Total: "698543"
+  },
+  {
+    Year: "1959",
+    Total: "786798"
+  },
+  {
+    Year: "1960",
+    Total: "878669"
+  },
+  {
+    Year: "1961",
+    Total: "883288"
+  },
+  {
+    Year: "1962",
+    Total: "951655"
+  },
+  {
+    Year: "1963",
+    Total: "1152065"
+  },
+  {
+    Year: "1964",
+    Total: "1319855"
+  },
+  {
+    Year: "1965",
+    Total: "1632833"
+  },
+  {
+    Year: "1966",
+    Total: "2048034"
+  },
+  {
+    Year: "1967",
+    Total: "2486213"
+  },
+  {
+    Year: "1968",
+    Total: "2958053"
+  },
+  {
+    Year: "1969",
+    Total: "3320576"
+  },
+  {
+    Year: "1970",
+    Total: "3341391"
+  },
+  {
+    Year: "1971",
+    Total: "3464174"
+  },
+  {
+    Year: "1972",
+    Total: "3915395"
+  },
+  {
+    Year: "1973",
+    Total: "4274286"
+  },
+  {
+    Year: "1974",
+    Total: "4410972"
+  },
+  {
+    Year: "1975",
+    Total: "4490668"
+  },
+  {
+    Year: "1976",
+    Total: "4912368"
+  },
+  {
+    Year: "1977",
+    Total: "5447648"
+  },
+  {
+    Year: "1978",
+    Total: "6185583"
+  },
+  {
+    Year: "1979",
+    Total: "6541820"
+  },
+  {
+    Year: "1980",
+    Total: "5213356"
+  },
+  {
+    Year: "1981",
+    Total: "5022152"
+  },
+  {
+    Year: "1982",
+    Total: "5630343"
+  },
+  {
+    Year: "1983",
+    Total: "6547439"
+  },
+  {
+    Year: "1984",
+    Total: "7173272"
+  },
+  {
+    Year: "1985",
+    Total: "7937806"
+  },
+  {
+    Year: "1986",
+    Total: "9084438"
+  },
+  {
+    Year: "1987",
+    Total: "9801030"
+  },
+  {
+    Year: "1988",
+    Total: "10748729"
+  },
+  {
+    Year: "1989",
+    Total: "11111080"
+  },
+  {
+    Year: "1990",
+    Total: "10937026"
+  },
+  {
+    Year: "1991",
+    Total: "11185920"
+  },
+  {
+    Year: "1992",
+    Total: "11759091"
+  },
+  {
+    Year: "1993",
+    Total: "11817706"
+  },
+  {
+    Year: "1994",
+    Total: "12681985"
+  },
+  {
+    Year: "1995",
+    Total: "12908395"
+  },
+  {
+    Year: "1996",
+    Total: "13461361"
+  },
+  {
+    Year: "1997",
+    Total: "13900712"
+  },
+  {
+    Year: "1998",
+    Total: "14340447"
+  },
+  {
+    Year: "1999",
+    Total: "14971261"
+  },
+  {
+    Year: "2000",
+    Total: "15746445"
+  },
+  {
+    Year: "2001",
+    Total: "14942061"
+  },
+  {
+    Year: "2002",
+    Total: "14731518"
+  },
+  {
+    Year: "2003",
+    Total: "15304975"
+  },
+  {
+    Year: "2004",
+    Total: "16517153"
+  },
+  {
+    Year: "2005",
+    Total: "17569355"
+  },
+  {
+    Year: "2006",
+    Total: "17673483"
+  },
+  {
+    Year: "2007",
+    Total: "18673441"
+  },
+  {
+    Year: "2008",
+    Total: "18419621"
+  },
+  {
+    Year: "2009",
+    Total: "17316835"
+  },
+  {
+    Year: "2010",
+    Total: "17205100"
+  },
+  {
+    Year: "2011",
+    Total: "17307916"
+  },
+  {
+    Year: "2012",
+    Total: "17682275"
+  },
+  {
+    Year: "2013",
+    Total: "18100313"
+  },
+  {
+    Year: "2014",
+    Total: "19019654"
+  },
+  {
+    Year: "2015",
+    Total: "20322360"
+  },
+  {
+    Year: "2016",
+    Total: "20969022"
+  },
+  {
+    Year: "2017",
+    Total: "22369589"
+  },
+  {
+    Year: "2018",
+    Total: "24273901"
   }
- 
- 
-  .foreground {
-    width: 100%;
-    margin: 0 auto;
-    height: auto;
-    position: relative;
-    outline: white 3px;
-  }
- 
- 
-  section {
-    height: 80vh;
-    background-color: white; /* 20% opaque */
-    /* color: white; */
-    outline: white 3px;
-    text-align: center;
-    max-width: 2000px; /* adjust at will */
-    color: black;
-    padding: 1em;
-    margin: 0 0 2em 0;}
-    .custom-heading {
-    font-family: Arial, sans-serif; /* Specify the desired font family */
-    font-size: 34px; /* Adjust font size as needed */
-    font-weight: bold; /* Optionally, adjust font weight */
-    color: #333; /* Optionally, specify font color */
-    text-align: center;
-  }
- 
- 
- </style>
- 
- 
- 
- 
+];
+
+let show = false;
+	// scales
+	const xScale = d3.scaleLinear()
+		.domain(d3.extent(data_lineGraph.map(d => d.Year)))
+		.range([5,95]);
+	
+	const yScale = d3.scaleLinear()
+		.domain(d3.extent(data_lineGraph.map(d => d.Total)))
+		.range([5,95]);
+	
+	// the path generator
+	const pathLine = d3.line()
+		.x(d => xScale(d.Year))
+		.y(d => yScale(d.Total))
+		.curve(d3.curveBasis);
+</script>
+<label for="show" style="display: inline;">Show Line:</label>
+<input id="show" type="checkbox" bind:checked={show} />
+<svg viewBox="0 0 300 300">
+	{#if (show)}
+		<path transition:draw={{duration: 2000}}
+					d={pathLine(data_lineGraph)} />
+	{/if}
+</svg>
+
+<style>
+	path {
+		stroke: purple;
+		stroke-width: 2;
+		fill: none;
+		stroke-linecap: round;
+	}
+</style>
---
+++ src/components/data.js
@@ -0,0 +1,10 @@
+export let data = [
+    {age: 24, temp: 37.2},
+    {age: 24.1, temp: 37.1},
+    {age: 24.2, temp: 36.8},
+    {age: 24.3, temp: 37.0},
+    {age: 24.4, temp: 36.7},
+    {age: 24.5, temp: 37.2},
+    {age: 24.6, temp: 36.5},
+    {age: 24.7, temp: 36.9},
+];
